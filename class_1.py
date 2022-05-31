fc25b72870af35c3dc2ea36933c0157036da32da
Maxim Kupelyan <maximkupelyan@Maxims-MacBook-Pro.local>
2022-05-31 21:26:23 +0400

    initial commit

---
+++ class_1.py
@@ -0,0 +1,40 @@
+# ex.
+# def drop_empty_items(dict_):
+#     empty_tuple = (None, '', [], {}, 0)
+#     for key in dict_.copy():
+#         if dict_[key] in empty_tuple:
+#             dict_.pop(key)
+#     return dict_
+
+#----------------------------------------------------------Git-and-Githab----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
